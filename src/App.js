9f24aa07b742e93070220a45725b70d98a2626ca
Farid1987 <farid3887@gmail.com>
2020-10-24 14:34:12 +0700

    add signup with email and password

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import ShopPage from "./pages/shop/shop.component";
 import Auth from "./pages/auth/auth.component";
 import Header from "./components/header/header.component";
 
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor() {
@@ -22,10 +22,25 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          }, () => {
+            console.log(this.state)
+          })
+        })
+      } else {
+        this.setState({currentUser: userAuth}, () => console.log(this.state))
+      }
+
+      
     });
   }
 
