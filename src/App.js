7fb4ddff37d9edc1d15e83085379d1df499407af
Farid1987 <farid3887@gmail.com>
2020-11-08 07:34:10 +0700

    add checkout page, checkout item component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Auth from "./pages/auth/auth.component";
+import CheckoutPage from "./pages/checkout/checkout.component";
 import Header from "./components/header/header.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { setCurrentUser } from './redux/user/user.actions.js';
@@ -46,6 +47,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
           <Route exact path="/auth" render={() => this.props.currentUser ? (<Redirect to="/"/>) : (<Auth/>)} />
         </Switch>
       </div>
