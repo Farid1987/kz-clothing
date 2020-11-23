24d236561137a10cac534d11f2ed63aee68bd7c2
Farid1987 <farid3887@gmail.com>
2020-11-23 11:19:54 +0700

    add stripe integration, update fav icon and title web

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_iEHZWvGwLWEAx7jWD0Nzy2HQ00SqS6C2OV';
+
+  const onToken = token => {
+    console.log(token)
+    alert('Payment Successful')
+  }
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='KZ Clothing'
+      billingAddress
+      shippingAddress
+      image='https://sendeyo.com/up/d/f3eb2117da'
+      description={`Yout total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  )
+}
+
+export default StripeCheckoutButton;
